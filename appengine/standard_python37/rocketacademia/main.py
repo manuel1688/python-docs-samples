53c5db34d24a8c383bd5cf2375d6a1c84629a95d
user <user@users-MacBook-Pro.local>
2020-01-31 01:09:56 -0500

    cupon de descuento

--- appengine/standard_python37/rocketacademia/main.py
+++ appengine/standard_python37/rocketacademia/main.py
@@ -15,20 +15,55 @@
 # [START gae_python37_app]
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from google.cloud import datastore
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    return render_template('index.html')
 
-    return render_template('index.html', times=dummy_times)
+@app.route('/descuento')
+def descuento():
+    return render_template('descuento.html')
+
+@app.route('/cupon')
+def cupon():
+    return render_template('cupon.html')
+
+@app.route('/obtener/descuento', methods=['GET', 'POST'])
+def form_example():
+    if request.method == 'POST':
+        nombre = request.form.get('nombre')
+        correo = request.form.get('correo')
+        telefono = request.form.get('telefono')
+
+        client = datastore.Client('rocketacademia')
+
+        query = client.query(kind='PROSPECTOS_CLIENTES')
+        query.add_filter('CORREO', '=', correo)
+        results = list(query.fetch())
+        if len(results) > 1:
+            return render_template("cupon_existente.html",correo=correo)
+
+        with client.transaction():
+            incomplete_key = client.key('PROSPECTOS_CLIENTES')
+
+            prospecto = datastore.Entity(key=incomplete_key)
+
+            prospecto.update({
+            'CORREO': correo,
+            'NOMBRE': nombre,
+            'TELEFONO': telefono,
+            'FECHA':datetime.datetime.now()
+            })
+
+            client.put(prospecto)
+
+        return render_template("cupon.html",form=[],nombre=nombre)
+
+    return render_template("descuento.html")
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
